e062f8a5abe89f3aab920a80cbbdf9d05fe04ad5
Pedro Enciso <penciso@ucsd.edu>
2021-05-25 16:49:05 -0700

    added rock paper scissors logic
    inot completely done, the user always chooses scissors.

--- main.js
+++ main.js
@@ -1,6 +1,25 @@
 const choices = ["rock", "paper", "scissors"];
+const playerSelection = "scissors";
 
 const computerPlay = () => {
   let randomNum = Math.floor(Math.random() * 3);
   return choices[randomNum];
 };
+
+const playRound = (playerSelection, computerSelection) => {
+  switch (playerSelection) {
+    case "scissors":
+      if (computerSelection === "rock") return "YOU LOSE. ROCK BEATS SCISSORS";
+      if (computerSelection === "scissors")
+        return "TIE. YOU BOTH CHOSE SCISSORS";
+      if (computerSelection === "paper")
+        return "YOU WIN! SCISSORS BEATS PAPER.";
+      break;
+  }
+};
+
+const computerSelection = computerPlay();
+
+const result = playRound(playerSelection, computerSelection);
+
+console.log(`The computer chose ${computerSelection}. ${result}`);
